c3cd5f5117a7b2f35d03cb5273ab1977bed59ea5
elevenqthree <elevenqtwo@gmail.com>
2024-02-07 23:02:12 +0300

    sleep raw data view

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.os.Environment
@@ -9,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity
 import java.io.File
 import java.io.IOException
 import java.nio.file.Path
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,48 +43,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun readFile(filePath: File): String {
-        if (!filePath.exists()) {
-            return "File not found."
-        }
+        if (!filePath.exists()) { return "File not found." }
 
         val database = SQLiteDatabase.openDatabase(
             filePath.path, null, SQLiteDatabase.OPEN_READONLY)
 
-        val tableNames = database.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null)
-            .use { cursor ->
-                mutableListOf<String>().apply {
-                    while (cursor.moveToNext()) {
-                        val tableName = cursor.getString(0)
-                        if (tableName != null) {
-                            add(tableName) } } } }
+        val tableName = "HUAMI_EXTENDED_ACTIVITY_SAMPLE"
 
         val fileContent = StringBuilder()
+        fileContent.append("\nTable: $tableName\n\n")
 
-        for (tableName in tableNames) {
-            fileContent.append("\nTable: $tableName\n\n")
-            val cursor2 = database.rawQuery("SELECT * FROM $tableName", null)
-            val columnNames = cursor2.columnNames
-            fileContent.append("Columns: ${columnNames.joinToString()}\n")
-            if (cursor2.moveToFirst()) {
-                do {
-                    val rowValues = mutableListOf<String>()
-                    for (columnName in columnNames) {
-                        val columnIndex = cursor2.getColumnIndex(columnName)
-                        if (columnIndex != -1) {
-                            rowValues.add(cursor2.getString(columnIndex) ?: "null")
-                        } else {
-                            rowValues.add("")
-                        }
-                    }
-                    fileContent.append("Row: ${rowValues.joinToString()}\n")
-                } while (cursor2.moveToNext())
-            } else {
-                fileContent.append("Table is empty.\n")
-            }
-            cursor2.close()
+        val sql = "SELECT TIMESTAMP, SLEEP FROM $tableName WHERE SLEEP > 0 ORDER BY TIMESTAMP DESC"
+
+        val cursor2 = database.rawQuery(sql, null)
+        val columnNames = cursor2.columnNames
+
+        fileContent.append("Columns: ${columnNames.joinToString(",    ")}\n")
+
+        if (!cursor2.moveToFirst()) {
+            fileContent.append("Table is empty or no rows with sleep > 0.\n")
         }
+
+        do {
+            val rowValues = mutableListOf<String>()
+            for (columnName in columnNames) {
+                val columnIndex = cursor2.getColumnIndex(columnName)
+                if (columnIndex != -1) {
+                    if (columnName == "TIMESTAMP") {
+                        val formattedTimestamp = getFormattedDateTime(cursor2.getLong(columnIndex))
+                        rowValues.add(formattedTimestamp)
+                    } else {
+                        rowValues.add(cursor2.getString(columnIndex) ?: "null")
+                    }
+                } else {
+                    rowValues.add("")
+                }
+            }
+            fileContent.append("${rowValues.joinToString(",    ")} \n")
+        } while (cursor2.moveToNext())
+
+        cursor2.close()
         database.close()
 
         return fileContent.toString()
+    }
+
+    private fun getFormattedDateTime(timestamp: Long): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+        return dateFormat.format(timestamp * 1000)
     }
 }
